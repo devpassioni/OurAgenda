64ee77bc889e857c4ce3f282f09190ece4f16b9b
Jonathan Passioni <jhow-rock@live.com>
2026-05-12 17:00:11 -0300

    commitando mudancas de controllers.

--- src/controllers/professionals.controller.ts
+++ src/controllers/professionals.controller.ts
@@ -1,10 +1,39 @@
 import {Request, Response, NextFunction} from 'express'
-import {z} from "zod"
+import {boolean, z} from "zod"
 import {v4 as uuid} from "uuid"
 import {db} from "../lib/db"
 
 const professionalSchema = z.object({
     name: z.string().min(2),
-    
+    bio: z.string().optional(),
 })
 
+
+export function list(req: Request, res: Response, next: NextFunction){
+    try{
+        const data =  db.prepare(`SELECT * FROM professionals WHERE is_active = 1`).all()
+        return res.json(data);
+        }       
+        catch(err){
+        next(err)
+    }
+}
+
+
+export function findById(req: Request, res: Response, next: NextFunction){
+    try{
+        const professionals = db.prepare(`SELECT * FROM professionals WHERE ID = ?`).get(req.params.id);
+        if(!professionals){
+            return res.json({message: "Profissional nao encontrado"})
+        }
+        return professionals;
+    }catch(err){
+        next(err)
+    }
+
+}
+
+export function create(req:Request, res: Response, next: NextFunction){
+    const professionals = professionalSchema.parse(req.body);
+    const id = uuid();
+}
--- src/lib/migration.ts
+++ src/lib/migration.ts
@@ -20,5 +20,47 @@ export function runMigrations() {
         price REAL NOT NULL,
         is_active INTEGER NOT NULL DEFAULT 1,
         created_at TEXT NOT NULL DEFAULT (datetime('now'))
-    );  `)
+            );  
+        CREATE TABLE IF NOT EXISTS professionals(
+        id TEXT PRIMARY KEY,
+        user_id TEXT FOREIGN KEY,
+        bio TEXT NOT NULL,
+        is_active INTEGER NOT NULL DEFAULT 1,
+        created_at TEXT NOT NULL DEFAULT (datetime('now'))
+        );
+        CREATE TABLE IF NOT EXISTS work_schedules(
+        id TEXT PRIMARY KEY,
+        professional_id TEXT FOREIGN_KEY,
+        day_of_week INTEGER NOT NULL,
+        start_time TEXT NOT NULL,
+        end_time TEXT NOT NULL,
+        lunch_start TEXT DEFAULT '12:00',
+        lunch_end TEXT DEFAULT '13:00');
+
+        
+        CREATE TABLE IF NOT EXISTS appointments(
+        id TEXT PRIMARY KEY,
+        client_id TEXT FOREIGN KEY,
+        professional_id TEXT FOREIGN KEY,
+        service_id TEXT FOREIGN KEY,
+        start_at TEXT NOT NULL,
+        end_at TEXT NOT NULL,
+        status TEXT NOT NULL,
+        notes TEXT,
+        created_at TEXT NOT NULL default(datetime('now'))
+        );
+
+        CREATE TABLE IF NOT EXISTS professional_services(
+        professional_id TEXT FOREIGN KEY,
+        service_id TEXT FOREIGN KEY,);
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    `)
     }
